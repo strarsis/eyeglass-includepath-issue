8d793fdaeee1f7500f7a375cd461b39f6100b589
strarsis <strarsis@gmail.com>
2016-11-14 21:50:40 +0100

    Add files.

---
+++ Gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var eyeglass = require("eyeglass");
+
+
+var sassOptions = {
+  // put node-sass options you need here.
+
+  eyeglass: {
+    // put eyeglass options you need here.
+  }
+};
+
+
+gulp.task("eyeglass", function () {
+  gulp.src("./scss/**/*.scss")
+    .pipe(sass(eyeglass(sassOptions)).on("error", sass.logError))
+    .pipe(gulp.dest("./css-eyeglass"));
+});
+
+
+gulp.task("sass", function() {
+  gulp.src("./scss/**/*.scss")
+      .pipe(sass(sassOptions).on("error", sass.logError))
+      .pipe(gulp.dest("./css"));
+});
