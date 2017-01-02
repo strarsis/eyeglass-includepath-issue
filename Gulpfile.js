2042f7c613b1e497cc38eda97309343faed3f939
strarsis <strarsis@gmail.com>
2017-01-02 16:10:22 +0100

    Add fix for include path issues.

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,19 +1,22 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
 var eyeglass = require("eyeglass");
+var sassIncludePaths = require("sass-include-paths");
 
 
 var sassOptions = {
   // put node-sass options you need here.
+  includePaths: [ "./scss", "../share-test" ],
 
   eyeglass: {
     // put eyeglass options you need here.
+    root: "./"
   }
 };
 
 
 gulp.task("eyeglass", function () {
-  sassOptions.includePaths = ['./scss'];
+  sassOptions.includePaths.push("./scss");
   gulp.src("./scss/**/*.scss")
     .pipe(sass(eyeglass(sassOptions)).on("error", sass.logError))
     .pipe(gulp.dest("./css-eyeglass"));
@@ -21,6 +24,7 @@ gulp.task("eyeglass", function () {
 
 
 gulp.task("sass", function() {
+  sassOptions.includePaths = sassOptions.includePaths.concat(sassIncludePaths.nodeModulesSync());
   gulp.src("./scss/**/*.scss")
       .pipe(sass(sassOptions).on("error", sass.logError))
       .pipe(gulp.dest("./css"));
